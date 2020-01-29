16c7a95e7f2a509a64ab43a519bab2cf87cf4382
Rugie Ann Barrameda <norugie015@gmail.com>
2020-01-29 14:29:32 -0800

    color buttons

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ resize();
 
 // resize canvas when window is resized
 function resize() {
-	ctx.canvas.width = window.innerWidth * 0.8;
-	ctx.canvas.height = window.innerHeight - 20;
+    ctx.canvas.width = window.innerWidth * 0.8;
+    ctx.canvas.height = window.innerHeight - 20;
 }
 
 // add event listeners to specify when functions should be triggered
@@ -22,24 +22,29 @@ var pos = { x: 0, y: 0 };
 
 // new position from mouse events
 function setPosition(e) {
-	pos.x = e.clientX;
-	pos.y = e.clientY;
+    pos.x = e.clientX;
+    pos.y = e.clientY;
+}
+
+function colorValue(e) {
+    var color = e.dataset.color;
+    console.log(color);
 }
 
 function draw(e) {
-	if (e.buttons !== 1) return; // if mouse is pressed.....
+    if (e.buttons !== 1) return; // if mouse is pressed.....
 
-	var color = "000";
+    var color = "000";
 
-	ctx.beginPath(); // begin the drawing path
+    ctx.beginPath(); // begin the drawing path
 
-	ctx.lineWidth = 5; // width of line
-	ctx.lineCap = "round"; // rounded end cap
-	ctx.strokeStyle = color; // hex color of line
+    ctx.lineWidth = 5; // width of line
+    ctx.lineCap = "round"; // rounded end cap
+    ctx.strokeStyle = color; // hex color of line
 
-	ctx.moveTo(pos.x, pos.y); // from position
-	setPosition(e);
-	ctx.lineTo(pos.x, pos.y); // to position
+    ctx.moveTo(pos.x, pos.y); // from position
+    setPosition(e);
+    ctx.lineTo(pos.x, pos.y); // to position
 
-	ctx.stroke(); // draw it!
+    ctx.stroke(); // draw it!
 }
