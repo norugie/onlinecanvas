2d36d34db439c135f2dbc90fbcc5841a0f158436
Rugie Ann Barrameda <norugie015@gmail.com>
2020-03-23 13:22:57 -0700

    adding canvas layers

--- js/canvas.js
+++ js/canvas.js
@@ -9,7 +9,7 @@ resize();
 // resize canvas when window is resized
 function resize() {
     ctx.canvas.width = window.innerWidth * 0.8;
-    ctx.canvas.height = window.innerHeight - 20;
+    ctx.canvas.height = window.innerHeight;
 }
 
 // add event listeners to specify when functions should be triggered
@@ -33,17 +33,14 @@ function colorValue(e) {
 }
 
 function draw(e) {
-    if (e.buttons !== 1) return; // if mouse is pressed.....
+    if (e.buttons !== 1) return; // if mouse is pressed
 
     ctx.beginPath(); // begin the drawing path
-
     ctx.lineWidth = 5; // width of line
     ctx.lineCap = "round"; // rounded end cap
     ctx.strokeStyle = color; // hex color of line
-
     ctx.moveTo(pos.x, pos.y); // from position
     setPosition(e);
     ctx.lineTo(pos.x, pos.y); // to position
-
     ctx.stroke(); // draw it!
 }
